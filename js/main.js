49bb202796e97e16250bb870ebe79df5d06148c8
KOFFICECILIA <mouaye.koffi@uvci.edu.ci>
2019-10-11 10:17:57 +0000

    ma super calculatrice

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// function aff(n){
+//     document.getElementById("ecrans").innerHTML += n;
+// }
+
+// function ce(){
+//     document.getElementById("ecrans").innerHTML = "" ;
+// }
+
+
+      var screen = document.querySelector('.ecrans')
+      
+     
+
